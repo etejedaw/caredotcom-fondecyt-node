60e079617e0d12844fc47eab20248951713d4887
Esteban Tejeda <etejedaw@hotmail.com>
2022-04-05 02:01:13 -0400

    app: Utilizando Wayback de prueba

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,11 @@
-import puppeteer from "puppeteer";
-import path from "path";
-import fs from "fs";
+import Wayback from "./utils/Wayback";
 
-const MAIN_URL = "https://web.archive.org/web/2021*/https://www.care.com/babysitters/new-york-ny";
+const URL = "https://www.care.com/babysitters/new-york-ny";
 
-async function main() {
-	await extractWeb();
-}
-
-const extractWeb = async () => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto(MAIN_URL);
-	const content = await page.content();
-
-	const dir = path.resolve(__dirname, "../public");
-	const saveDir = `${dir}/page.html`;
-	if(!fs.existsSync(dir)) fs.mkdirSync(dir);
-	fs.writeFileSync(saveDir, content);
-	console.log(`saved in ${saveDir}`);
-
-	await browser.close();
-	console.log("navegador cerrado");
+const main = async () => {
+	const wayback = new Wayback(URL);
+	const links = await wayback.getUrls();
+	console.log(links);
 };
 
 main();
