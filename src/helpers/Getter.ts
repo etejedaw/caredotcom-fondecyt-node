079328da21d3840864cfbc0bae8a87d28eb04193
Esteban Tejeda <etejedaw@hotmail.com>
2022-04-07 17:25:42 -0400

    Getter.ts: Creada clase encargada de extraer el html de las páginas webs

---
+++ src/helpers/Getter.ts
@@ -0,0 +1,25 @@
+import request from "request-promise";
+import fs from "fs";
+import path from "path";
+
+class Getter {
+	readonly #url: string;
+	readonly #html: string;
+
+	private constructor(url: string, html: string) {
+		this.#url = url;
+		this.#html = html;
+	}
+
+	static async build(url: string): Promise<Getter>{
+		const html = await request.get(url);
+		return new Getter(url, html);
+	}
+
+	get html(){
+		return this.#html;
+	}
+
+}
+
+export default Getter;
