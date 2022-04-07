e0d7cbddc384a90aca11caf1006c568eb462b0a7
Esteban Tejeda <etejedaw@hotmail.com>
2022-04-07 16:22:00 -0400

    app: Se actualiza el método main() para utilizar de manera dinámica las clases GenerateUri y Wayback

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,18 @@
 import Wayback from "./utils/Wayback";
+import GenerateUri from "./utils/GenerateUri";
 
 const URL = "https://www.care.com";
 
 const main = async () => {
 
-	const links = [
-		"https://www.care.com/child-care/chicago-il",
-		"https://www.care.com/child-care/new-work-ny",
-		"https://www.care.com/babysitters/chicago-il",
-		"https://www.care.com/babysitters/new-work-ny"
-	];
+	const generateUri = new GenerateUri(URL); //Genera las combinaciones de links
 
-	const wayback_1 = new Wayback(links[1]);
-	const urls_1 = await wayback_1.getList();
-	console.log(urls_1[0]);
-
-	await sleep(4000);
-
-	const wayback_0 = new Wayback(links[0]);
-	const urls_0 = await wayback_0.getList();
-	console.log(urls_0[0]);
-
-	await sleep(4000);
-
-	const wayback_2 = new Wayback(links[2]);
-	const urls_2 = await wayback_2.getList();
-	console.log(urls_2[0]);
+	for (const link of generateUri.getLinks()) {
+		const wayback = new Wayback(link); //Solicita las URI en Wayback Machine
+		const list = await wayback.getList();
+		if(list.length !== 0)  console.log(list[0]);
+		await sleep(4000);
+	}
 
 };
 
