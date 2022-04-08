00a5ebab6812986999b33679b00c06619e207291
Esteban Tejeda <etejedaw@hotmail.com>
2022-04-08 11:21:44 -0400

    Save.ts: Se agregó la función loadHTML() que permite cargar un archivo HTML y retornarlo como string

--- src/utils/Save.ts
+++ src/utils/Save.ts
@@ -11,6 +11,13 @@ class Save {
 		console.log("HTML saved");
 	}
 
+	static loadHTML(name: string): string{
+		const dir = path.resolve(__dirname, "../public");
+		const file = `${dir}/${name}.html`;
+		if(!fs.existsSync(file)) throw new Error("File not found");
+		return fs.readFileSync(file, "utf8");
+	}
+
 }
 
 export default Save;
