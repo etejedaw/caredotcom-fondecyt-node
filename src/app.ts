c0a9ac73a2852bfa564796e5b9a1f76b71a50b58
Esteban Tejeda <etejedaw@hotmail.com>
2022-04-07 21:39:03 -0400

    app.ts: Eliminado comentarios innecesarios y reducido el tiempo de sleep()

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ const URL = "https://www.care.com";
 const main = async () => {
 
 	const generateUri = new GenerateUri(URL); //Genera las combinaciones de links
+
 	let count = 0;
 
 	for (const link of generateUri.getLinks()) {
@@ -20,43 +21,11 @@ const main = async () => {
 				const page = await Getter.build(pageUrl);
 				Save.saveHtml(page.html, `page-${count}`);
 				count++;
-				await Sleep.sleep(4000);
+				await Sleep.sleep(2000);
 			}
 		}
 		await Sleep.sleep(4000);
 	}
-
-/*
-	const generateUri = new GenerateUri(URL);
-
-	// Example 1
-
-	const uri1 = generateUri.getLinks()[0];
-	const wayback1 = new Wayback(uri1);
-	const links1 = await wayback1.getList();
-	const link1 = links1[0].uri;
-
-	const page1 = await Getter.build(link1);
-
-	Save.saveHtml(page1.html, "page1");
-
-	//
-	await Sleep.sleep(4000);
-	//
-	//Example 2
-
-	const uri2 = generateUri.getLinks()[2];
-	const wayback2 = new Wayback(uri2);
-	const links2 = await wayback2.getList();
-	const link2 = links2[0].uri;
-
-	const page2 = await Getter.build(link2);
-
-	Save.saveHtml(page2.html, "page2");
-
-
-
- */
 };
 
 main();
