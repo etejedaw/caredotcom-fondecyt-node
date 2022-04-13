61afac3c66faf53531604eb633e8698110dd237f
Esteban Tejeda <etejedaw@hotmail.com>
2022-04-12 20:35:54 -0400

    helpers/Scrape.ts: Añadida nueva forma de realizar scraping por problemas de compatibilidad entre las distintas versiones de la página

--- src/helpers/Scrape.ts
+++ src/helpers/Scrape.ts
@@ -8,8 +8,14 @@ class Scrape {
 	}
 
 	extractData() {
+		let data = this.extractData1();
+		if(data.length === 0) data = this.extractData2();
+		return data;
+	}
+
+	extractData1() {
 		const $ = cheerio.load(this.#html);
-		const data = $(".group .search-result").map((idx, elem) => {
+		const data = $(".group .search-result").map((_idx, elem) => {
 			const name = $(elem).find(".profile-link").text().trim();
 			const priceRank = $(elem).find(".rate-ct").text().trim();
 			const experience = $(elem).find(".block2").text().trim();
@@ -18,6 +24,20 @@ class Scrape {
 		}).get();
 		return data;
 	}
+
+	extractData2() {
+		const $ = cheerio.load(this.#html);
+		const data = $(".individual-info").map((_idx, elem) => {
+			const name = $(elem).find(".pro-title").text().trim();
+			const priceRank = $(elem).find(".provider-detail-info .row .col-xs-5").text().trim();
+			const experience = $(elem).find(".provider-detail-info .row .col-xs-7").text().trim();
+			const age = $(elem).find(".provider-detail-info .row .hidden-xs").text().trim();
+			return {name, priceRank, experience, age};
+		}).get();
+		return data;
+	}
+
+
 
 }
 
