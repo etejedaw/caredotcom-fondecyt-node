4d911014928ea9cbd401193009e43baf9bc026e9
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-01 22:05:12 -0400

    config/Environment.ts: Se eliminó la variable STATES ya que no se utilizará. Se solucionó error en variable de prueba para LOCAL_AREAS

--- src/config/Environment.ts
+++ src/config/Environment.ts
@@ -1,18 +1,12 @@
 class Environment {
-	readonly #STATES: Array<string>;
 	readonly #LOCAL_AREAS: Array<string>;
 	readonly #OFFERS: Array<string>;
 	readonly #JOBS: Array<string>;
 
 	constructor() {
-		this.#STATES = ["al", "ak"];
-		this.#LOCAL_AREAS = ["chicago-il", "new-work-ny"];
+		this.#LOCAL_AREAS = ["chicago-il", "new-york-ny"];
 		this.#OFFERS = ["child-care", "babysitters"];
 		this.#JOBS = ["nanny-jobs", "senior-care-jobs"];
-	}
-
-	get STATES() {
-		return this.#STATES;
 	}
 
 	get LOCAL_AREAS() {
