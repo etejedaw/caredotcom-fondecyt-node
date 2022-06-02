97459497c5e5330b4199846495dcf51e7dd9de23
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-02 01:25:48 -0400

    helpers/Wayback.ts: Creada función checkRepeat(...) encargada de verificar que solo se obtenga un link por día con el fin de evitar duplicados. La función será utilizada por el método getList() que entrega la data normalizada. Creada función getFullList() la cual entrega la data completa, sin evitar duplicados por fecha.

--- src/helpers/Wayback.ts
+++ src/helpers/Wayback.ts
@@ -8,24 +8,51 @@ class Wayback {
 		this.#uri = url;
 	}
 
+	async getList(): Promise<List[]> {
+		const fullList = await this.getFullList();
+		return this.#checkRepeat(fullList);
+	}
+
+	async getFullList(): Promise<List[]> {
+		try{
+			const getUrl = await this.#getUrls();
+			return this.#normalizeDate(getUrl.mementos.list);
+		}
+		catch (error) {
+			return [] as List[];
+		}
+	}
+
 	async #getUrls(): Promise<TimeTravel> {
 		const link = `http://timetravel.mementoweb.org/timemap/json/${this.#uri}`;
 		const requestInit: RequestInit = {
 			method: "GET",
-			headers: {"Content-Type": "application/json"}
+			headers: {"Content-Type": "application/json"},
 		};
 		const response = await fetch(link, requestInit);
 		return await response.json();
 	}
 
-	async getList(): Promise<List[]> {
-		try{
-			const timeTravel = await this.#getUrls();
-			return timeTravel.mementos.list;
-		}
-		catch (error) {
-			return [] as List[];
-		}
+	#normalizeDate(list: List[]): List[] {
+		const newList = list.map(data => {
+			const uri = data.uri;
+			const datetime = new Date(data.datetime);
+			return {uri, datetime};
+		});
+		return [...newList];
+	}
+
+	#checkRepeat(list: List[]): List[] {
+		const newList = list.filter((item, index, self) => {
+			const datetime = item.datetime;
+			const formatDate = `${datetime.getFullYear()}-${datetime.getUTCMonth()+1}-${datetime.getUTCDate()}`;
+			return index === self.findIndex(t => {
+				const newDatetime = t.datetime;
+				const newFormatDate =`${newDatetime.getFullYear()}-${newDatetime.getUTCMonth()+1}-${newDatetime.getUTCDate()}`;
+				return formatDate === newFormatDate;
+			});
+		});
+		return newList;
 	}
 
 }
