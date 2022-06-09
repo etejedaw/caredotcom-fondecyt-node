adcb1a34fb3fe95e63bda332bf74928915b5e05d
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-09 00:07:47 -0400

    jest.config.ts: Se excluyó la carpeta 'dist' de los tests. package.json: Se modificó el comando 'test'.

--- jest.config.ts
+++ jest.config.ts
@@ -85,7 +85,10 @@ export default {
 	// moduleNameMapper: {},
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-	// modulePathIgnorePatterns: [],
+	modulePathIgnorePatterns: [
+		"/node_modules/",
+		"/dist/"
+	],
 
 	// Activates notifications for test results
 	// notify: false,
