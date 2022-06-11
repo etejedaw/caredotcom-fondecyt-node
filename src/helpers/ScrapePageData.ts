1ba52da16f9b2f8a745f5679e19d18fbc2635282
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-11 02:31:40 -0400

    helpers/ScrapePageData.ts: Creado método extractDataV1() encargado del primer prototipo de extracción para PageData

--- src/helpers/ScrapePageData.ts
+++ src/helpers/ScrapePageData.ts
@@ -1,4 +1,6 @@
 import PageData from "../interfaces/PageData";
+import cheerio from "cheerio";
+import ObjectExtended from "../utils/ObjectExtended";
 
 class ScrapePageData {
 	readonly #html: string;
@@ -8,12 +10,28 @@ class ScrapePageData {
 	}
 
 	extract(): PageData {
-		return {} as PageData;
+		let extract = this.extractDataV1();
+		if(ObjectExtended.isEmpty(extract)) extract = this.extractDataV2();
+		return extract;
 	}
 
 	extractDataV1(): PageData {
+		const $ = cheerio.load(this.#html);
+		const regex = /(\d[\d.|,]*)/g;
+		const data = $("h3.college-sub-head-wrap").text();
+		const pageData = data.match(regex);
+		if(!pageData) return {} as PageData;
+		const providersTemp = pageData[0].replace(",", "");
+		const providers = parseInt(providersTemp);
+		const average = parseFloat(pageData[1]);
+		const pageNumber = 0;
+		return {pageNumber, providers, average};
+	}
+
+	extractDataV2(): PageData {
 		return {} as PageData;
 	}
+
 }
 
 export default ScrapePageData;
