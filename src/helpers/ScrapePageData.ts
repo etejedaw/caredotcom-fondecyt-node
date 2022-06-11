496b09ff0198c55ee129d56c9836f09cffd0bf56
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-11 11:57:42 -0400

    helpers/ScrapePageData.ts: Se añadieron nuevas funciones para que se extraiga providers y average de forma separada a pageNumber

--- src/helpers/ScrapePageData.ts
+++ src/helpers/ScrapePageData.ts
@@ -1,6 +1,7 @@
 import PageData from "../interfaces/PageData";
 import cheerio from "cheerio";
 import ObjectExtended from "../utils/ObjectExtended";
+import extraData from "../interfaces/ExtraData";
 
 class ScrapePageData {
 	readonly #html: string;
@@ -10,12 +11,18 @@ class ScrapePageData {
 	}
 
 	extract(): PageData {
+		const data = this.extractData();
+		const pages = this.extractPage();
+		return Object.assign(data, pages);
+	}
+
+	extractData(): {providers: number, average: number} {
 		let extract = this.extractDataV1();
 		if(ObjectExtended.isEmpty(extract)) extract = this.extractDataV2();
 		return extract;
 	}
 
-	extractDataV1(): PageData {
+	extractDataV1(): {providers: number, average: number} {
 		const $ = cheerio.load(this.#html);
 		const regex = /(\d[\d.|,]*)/g;
 		const data = $("h3.college-sub-head-wrap").text();
@@ -24,12 +31,27 @@ class ScrapePageData {
 		const providersTemp = pageData[0].replace(",", "");
 		const providers = parseInt(providersTemp);
 		const average = parseFloat(pageData[1]);
-		const pageNumber = 0;
-		return {pageNumber, providers, average};
+		return {providers, average};
 	}
 
-	extractDataV2(): PageData {
+	extractDataV2(): {providers: number, average: number} {
 		return {} as PageData;
+	}
+
+	extractPage(): {pageNumber: number} {
+		let extract = this.extractPageV1();
+		if (ObjectExtended.isEmpty(extract)) extract = this.extractPageV2();
+		return extract;
+	}
+
+	extractPageV1(): {pageNumber: number} {
+		const pageNumber = 0;
+		return {pageNumber};
+	}
+
+	extractPageV2(): {pageNumber: number} {
+		const pageNumber = 0;
+		return {pageNumber};
 	}
 
 }
