3c2ab04b9fc24d48285f835a1ebd4461080e9013
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-11 13:02:19 -0400

    helpers/ScrapePageData.ts: Finalizada las distintas extracciones en extractData()

--- src/helpers/ScrapePageData.ts
+++ src/helpers/ScrapePageData.ts
@@ -19,6 +19,8 @@ class ScrapePageData {
 	extractData(): {providers: number, average: number} {
 		let extract = this.extractDataV1();
 		if(ObjectExtended.isEmpty(extract)) extract = this.extractDataV2();
+		if(ObjectExtended.isEmpty(extract)) extract = this.extractDataV3();
+		if(ObjectExtended.isEmpty(extract)) extract = this.extractDataV4();
 		return extract;
 	}
 
@@ -40,6 +42,32 @@ class ScrapePageData {
 		//return numbers by ',' or '.'
 		const regex = /(\d[\d.|,]*)/g;
 		const data = $("h3.college-sub-head-wrap").text();
+		const pageData = data.match(regex);
+		if(!pageData) return {} as PageData;
+		const providersTemp = pageData[0].replace(",", "");
+		const providers = parseInt(providersTemp);
+		const average = parseFloat(pageData[1]);
+		return {providers, average};
+	}
+
+	extractDataV3(): {providers: number, average: number} {
+		const $ = cheerio.load(this.#html);
+		//return numbers by ',' or '.'
+		const regex = /(\d[\d.|,]*)/g;
+		const data = $(".heroImg .body-1").text();
+		const pageData = data.match(regex);
+		if(!pageData) return {} as PageData;
+		const providersTemp = pageData[0].replace(",", "");
+		const providers = parseInt(providersTemp);
+		const average = parseFloat(pageData[1]);
+		return {providers, average};
+	}
+
+	extractDataV4(): {providers: number, average: number} {
+		const $ = cheerio.load(this.#html);
+		//return numbers by ',' or '.'
+		const regex = /(\d[\d.|,]*)/g;
+		const data = $(".MuiGrid-root.MuiGrid-container .MuiGrid-root.MuiGrid-item [role='dialog'] [data-testid='sub-text']").text();
 		const pageData = data.match(regex);
 		if(!pageData) return {} as PageData;
 		const providersTemp = pageData[0].replace(",", "");
