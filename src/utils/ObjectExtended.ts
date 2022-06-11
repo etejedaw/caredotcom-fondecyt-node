3420397a11705c92d934423ae8f86b757f47536e
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-11 11:56:08 -0400

    utils/ObjectExtended.ts: Se añadió método hasAllValues(...)

--- src/utils/ObjectExtended.ts
+++ src/utils/ObjectExtended.ts
@@ -1,4 +1,8 @@
 class ObjectExtended {
+	static hasAllValues(object: object, length: number): boolean {
+		return Object.keys(object).length === length;
+	}
+
 	static isEmpty(object: object) {
 		return Object.keys(object).length === 0;
 	}
