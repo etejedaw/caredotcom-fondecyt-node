6eb7c1130c5a3dfbe050d771af332cc2d7ddeb01
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-20 18:23:30 -0400

    helpers/GenerateUri.ts: Se actualizó para utilizar los métodos estáticos de Environment

--- src/helpers/GenerateUri.ts
+++ src/helpers/GenerateUri.ts
@@ -1,29 +1,26 @@
 import Environment from "../config/Environment";
 import UriData from "../interfaces/UriData";
-import uriData from "../interfaces/UriData";
 import DataType from "../enum/DataType";
 
 class GenerateUri {
 	readonly #baseUrl: string;
-	readonly #environment: Environment;
 
 	constructor(baseUrl: string) {
 		this.#baseUrl = baseUrl;
-		this.#environment = new Environment();
 	}
 
 	getOfferLinks(): Array<UriData> {
-		const links = this.#environment.OFFERS.map(offerName => {
-			return this.#environment.LOCAL_AREAS.map(localArea => this.#uriCombinations(offerName, localArea, DataType.OFFER));
+		const links = Environment.OFFERS.map(offerName => {
+			return Environment.LOCAL_AREAS.map(localArea => this.#uriCombinations(offerName, localArea, DataType.OFFER));
 		});
-		return links.flat() as Array<uriData>;
+		return links.flat();
 	}
 
 	getJobsLinks(): Array<UriData> {
-		const links = this.#environment.JOBS.map(jobName => {
-			return this.#environment.LOCAL_AREAS.map(localArea => this.#uriCombinations(jobName, localArea, DataType.JOB));
+		const links = Environment.JOBS.map(jobName => {
+			return Environment.LOCAL_AREAS.map(localArea => this.#uriCombinations(jobName, localArea, DataType.JOB));
 		});
-		return links.flat() as Array<uriData>;
+		return links.flat();
 	}
 
 	#uriCombinations(name: string, localArea: string, dataType: DataType): UriData {
