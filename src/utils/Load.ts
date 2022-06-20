eef680886dd87e716832aba45f976a26684ef187
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-20 18:21:59 -0400

    utils/Load.ts: Se agregó el método txt(...) que permite cargar un archivo de texto y retornarlo como un array

--- src/utils/Load.ts
+++ src/utils/Load.ts
@@ -9,6 +9,13 @@ class Load {
 		return fs.readFileSync(file, "utf8");
 	}
 
+	static txt(name: string, dir?: string): Array<string> {
+		dir = dir || path.resolve(__dirname, "../public");
+		const file = `${dir}/${name}.txt`;
+		if(!fs.existsSync(file)) throw new Error("File not found");
+		return fs.readFileSync(file, "utf-8").toString().split("\n");
+	}
+
 }
 
 export default Load;
