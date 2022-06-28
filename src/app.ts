cfe49ff4596c58bd1e0e3cfad3972c35d036da33
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-28 02:17:22 -0400

    Se añadieron mensajes de finalización

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,12 @@ const main = async () => {
 	switch (Environment.DATA_TYPE) {
 		case DataType.JOB: {
 			await GetData.initJobs();
+			console.info("Jobs Extraction Successfully");
 			break;
 		}
 		case DataType.OFFER: {
 			await GetData.initOffers();
+			console.info("Offers Extraction Successfully");
 			break;
 		}
 		default: throw new Error("Invalid Option");
