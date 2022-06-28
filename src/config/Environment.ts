df2dd17db6e016df540694f13474e63b57fa50a6
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-28 01:11:00 -0400

    config/Environment.ts: Separada variable PUBLIC_DIR en OUTPUT_DIR e INPUT_DIR

--- src/config/Environment.ts
+++ src/config/Environment.ts
@@ -4,21 +4,21 @@ import path from "path";
 class Environment {
 
 	static get LOCAL_AREAS(): Array<string> {
-		const dir = this.PUBLIC_DIR;
+		const dir = this.INPUT_DIR;
 		const data = Load.txt("local_areas", dir);
 		data.pop();
 		return [...data];
 	}
 
 	static get OFFERS(): Array<string> {
-		const dir = this.PUBLIC_DIR;
+		const dir = this.INPUT_DIR;
 		const data = Load.txt("offers", dir);
 		data.pop();
 		return [...data];
 	}
 
 	static get JOBS(): Array<string> {
-		const dir = this.PUBLIC_DIR;
+		const dir = this.INPUT_DIR;
 		const data = Load.txt("jobs", dir);
 		data.pop();
 		return [...data];
@@ -28,8 +28,12 @@ class Environment {
 		return "https://www.care.com";
 	}
 
-	static get PUBLIC_DIR(): string {
-		return path.resolve(__dirname, "../public");
+	static get OUTPUT_DIR(): string {
+		return path.resolve(__dirname, "../output");
+	}
+
+	static get INPUT_DIR(): string {
+		return path.resolve(__dirname, "../../../src/public");
 	}
 
 }
