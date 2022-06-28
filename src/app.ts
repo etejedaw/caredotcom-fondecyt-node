ed59915c0f1867c605abae4672c3a1c0e61c0684
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-28 01:49:54 -0400

    helpers/GetData.ts: Movida extracción del main a dos funciones app.ts: Función main() ahora discrimina sobre el tipo de extracción en base a la variable de entorno de la clase Environment

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,20 @@
-import GenerateUri from "./helpers/GenerateUri";
-import Wayback from "./helpers/Wayback";
-import ExtraData from "./interfaces/ExtraData";
-import Getter from "./helpers/Getter";
-import Scrape from "./helpers/Scrape";
-import Sleep from "./utils/Sleep";
-import MergeData from "./types/MergeData";
-import ArrayExtended from "./utils/ArrayExtended";
-import Save from "./utils/Save";
+import DataType from "./enum/DataType";
+import GetData from "./helpers/GetData";
 import Environment from "./config/Environment";
-import SaveType from "./types/SaveType";
 
 const main = async () => {
-	const generateUri = new GenerateUri(Environment.URL);
-	const offerLinks = generateUri.getOfferLinks();
-	let allData = [] as MergeData[];
-	for (const offer of offerLinks) {
-		const uri = offer.uri;
-		const localArea = offer.localArea;
-		const information = offer.information;
-		const wayback = new Wayback(uri);
-		const list = await wayback.getList();
-		if(list.length !== 0) {
-			for (const link of list) {
-				const url = link.uri;
-				const date = link.datetime;
-				const extraData = {localArea, date, information} as ExtraData;
-				await Sleep.sleep(4000);
-				const getter = await Getter.build(url);
-				const html = getter.html;
-				if (html) {
-					const scrape = new Scrape(html, extraData);
-					const data = scrape.getMergeData();
-					allData = allData.concat(data);
-					const csv = ArrayExtended.jsonToCsv(allData);
-					const saveType = {
-						data: csv,
-						name: "offers",
-						dir: Environment.OUTPUT_DIR
-					} as SaveType;
-					Save.toCsv(saveType);
-					console.info("CSV Actualizado");
-				} else console.error(`CANT EXTRACT: ${url}`);
-			}
+	switch (Environment.DATA_TYPE) {
+		case DataType.JOB: {
+			await GetData.initJobs();
+			break;
 		}
-		else console.error(`NOT FOUND: ${uri}`);
+		case DataType.OFFER: {
+			await GetData.initOffers();
+			break;
+		}
+		default: throw new Error("Invalid Option");
 	}
+
 };
 
 main();
