ec73541a4f51efff37a3a951ffa269131a8637ac
Esteban Tejeda <etejedaw@hotmail.com>
2022-06-28 22:25:11 -0400

    types/Data.ts & types/MergeData.ts: Se dejaron los types con generics para que puedan ser reutilizados tanto en Offer como en Jobs

---
+++ src/types/Data.ts
@@ -0,0 +1,9 @@
+import ExtraData from "../interfaces/ExtraData";
+
+type Data<Provider, PageData> = {
+    provider: Provider[];
+    pageData: PageData;
+    extraData: ExtraData;
+}
+
+export default Data;
---
+++ src/types/MergeData.ts
@@ -0,0 +1,8 @@
+import ExtraData from "../interfaces/ExtraData";
+
+type MergeData<Provider, PageData> =
+	& Provider
+	& PageData
+	& ExtraData;
+
+export default MergeData;
