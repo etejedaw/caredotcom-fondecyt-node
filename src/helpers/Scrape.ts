9a9c69194e7c4355de93935ebe378f5f73626526
Esteban Tejeda <etejedaw@hotmail.com>
2022-08-09 12:50:19 -0400

    helpers/Scrape.ts: Se creó la extracción para obteners Jobs

--- src/helpers/Scrape.ts
+++ src/helpers/Scrape.ts
@@ -11,6 +11,9 @@ import Save from "../utils/Save";
 import MergeData from "../types/MergeData";
 import OfferProvider from "../interfaces/OfferProvider";
 import OfferPageData from "../interfaces/OfferPageData";
+import ScrapeJobs from "./ScrapeJobs";
+import JobProvider from "../interfaces/JobProvider";
+import JobPageData from "../interfaces/JobPageData";
 
 class Scrape {
 	static async initOffers() {
@@ -51,7 +54,41 @@ class Scrape {
 	}
 
 	static async initJobs() {
-
+		const generateUri = new GenerateUri(Environment.URL);
+		const jobLinks = generateUri.getJobsLinks();
+		let allData = [] as MergeData<JobProvider, JobPageData>[];
+		for (const job of jobLinks) {
+			const uri = job.uri;
+			const localArea = job.localArea;
+			const information = job.information;
+			const wayback = new Wayback(uri);
+			const list = await wayback.getList();
+			if (list.length !== 0) {
+				for (const link of list) {
+					const url = link.uri;
+					const date = link.datetime;
+					const extraData = {localArea, date, information} as ExtraData;
+					await Sleep.sleep(4000);
+					const getter = await Getter.build(url);
+					const html = getter.html;
+					if(html) {
+						const scrape = new ScrapeJobs(html, extraData);
+						const data = scrape.getMergeData();
+						allData = allData.concat(data);
+						const csv = ArrayExtended.jsonToCsv(allData);
+						const saveType = {
+							data: csv,
+							name: "jobs",
+							dir: Environment.OUTPUT_DIR
+						} as SaveType;
+						Save.toCsv(saveType);
+						console.info("CSV Updated");
+					}
+					else console.error(`CANT EXTRACT: ${url}`);
+				}
+			}
+			else console.error(`NOT FOUND: ${uri}`);
+		}
 	}
 
 }
