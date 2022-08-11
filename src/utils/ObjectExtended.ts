1a08fc78df551ee8dd2136a22d4fe1ce81e91626
Esteban Tejeda <etejedaw@hotmail.com>
2022-08-11 00:09:30 -0400

    utils/ObjectExtended.ts: Creado método hasEmptyValues encargado de ver si todos los valores de un objecto son distintos de un empty array

--- src/utils/ObjectExtended.ts
+++ src/utils/ObjectExtended.ts
@@ -1,10 +1,16 @@
 class ObjectExtended {
-	static hasAllValues(object: object, length: number): boolean {
-		return Object.keys(object).length === length;
+
+	static hasAllValues(obj: object, length: number): boolean {
+		return Object.keys(obj).length === length;
 	}
 
-	static isEmpty(object: object) {
-		return Object.keys(object).length === 0;
+	static isEmpty(obj: object) {
+		return Object.keys(obj).length === 0;
+	}
+
+	static hasEmptyValues(obj: object): boolean {
+		const keys = Object.keys(obj) as Array<keyof object>;
+		return keys.some(key => obj[key] === "");
 	}
 }
 
