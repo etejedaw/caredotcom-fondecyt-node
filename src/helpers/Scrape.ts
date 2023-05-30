48faffc4762e0693298d9d57ba5b358d45fd06f6
Esteban Tejeda <etejedaw@hotmail.com>
2023-05-30 02:08:35 -0400

    chore: improve logger messages

--- src/helpers/Scrape.ts
+++ src/helpers/Scrape.ts
@@ -7,6 +7,7 @@ class Scrape {
 	static async init(dataType: DataType) {
 		const dataLinks = ScrapeHelper.getLinks(dataType);
 		for (const data of dataLinks) {
+			console.info(`Searching Archive links in: ${data.uri}`);
 			const list = await ScrapeHelper.getWaybackList(data.uri);
 			ScrapeHelper.createCsv(
 				dataType,
@@ -29,9 +30,9 @@ class Scrape {
 						const dir = ScrapeHelper.generateDir(dataType, data.information);
 						ScrapeHelper.addToCsv(dir, csv);
 						console.info(`CSV Updated. Extracted: ${link.uri}`);
-					} else console.error(`CANT EXTRACT: ${link.uri}`);
+					} else console.error(`Can't Extract: ${link.uri}`);
 				}
-			} else console.error(`NOT FOUND: ${data.uri}`);
+			} else console.error(`Not Found: ${data.uri}`);
 		}
 	}
 }
